7e1cd24de2e695de9ea255a0a02386cbd61ffad1
Igtur <ig.turtsov@gmail.com>
2022-12-24 14:58:30 +0300

    add TodoItem component

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import TodoItem from "./Todoitem"
+
+const todos = [
+    {
+       _id: '213hdss1jgjh',
+       title: 'Finish the essay collaboration',
+       isCompleted: false,
+    },
+    {
+        _id: '213h1jgjkjkjhh',
+       title: 'Read book',
+       isCompleted: false,
+    },
+    {
+        _id: '213h32ewqe1jgjh',
+       title: 'Skip this video',
+       isCompleted: false,
+    },
+]
 
 const Home = () => {
     return (
-        <div className="">
-            Home
-        </div>
+    <div className="bg-gray-900 h-screen text-white">
+    {todos.map(todo => (
+       <TodoItem key={todo._id} todo={todo}/>
+    ))}
+    </div>
     )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+   //    (1) import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './components/screens/Home/Home'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// (1) const root = ReactDOM.createRoot(document.getElementById('root'));
+//    root.render(
+//    <React.StrictMode>
+//     <Home />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
+ReactDOM.render(
+    <React.StrictMode>
+      <Home />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
