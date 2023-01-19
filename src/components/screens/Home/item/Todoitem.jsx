18b44195158b38783acf9faa429ee82392406453
Igtur <ig.turtsov@gmail.com>
2023-01-19 10:15:51 +0300

    add style trash

--- src/components/screens/Home/item/Todoitem.jsx
+++ src/components/screens/Home/item/Todoitem.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Check from "./Check";
-
+import cn from 'classnames';
+import {BsTrash} from 'react-icons/bs'
  
 
 const TodoItem = ({todo, changeTodo}) => {
-    return (<button className='flex items-center mb-2 rounded-2xl bg-gray-800 p-5 w-full'
+    return (
+    <button 
+    className='flex items-center justify-between mb-2 rounded-2xl bg-gray-800 p-5 w-full'
     onClick={()=>changeTodo(todo._id)}
     >
-     <Check isCompleted={todo.isCompleted}/>
-    {todo.title}
-    </button>)
+    <span className='flex items-center'>
+        <Check isCompleted={todo.isCompleted}/>
+    <span 
+        className={cn({'line-through':todo.isCompleted})}>{todo.title}
+    </span>
+    </span>
+        <BsTrash size={22} className='text-gray-400 hover:text-red-400'/>
+    </button>
+    )
 }
 
 export default TodoItem 
